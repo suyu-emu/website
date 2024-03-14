c2fcf98c88f3ed0b5061b6a62d3a688c04b96369
not-nullptr <needhelpwithrift@gmail.com>
2024-03-14 15:58:52 +0000

    fix starcount

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,51 +4,51 @@ import { DISCORD_USER_TOKEN, GITLAB_API_TOKEN } from "$env/static/private";
 let memberCount = 0;
 let starCount = 0;
 let roleMembers = {
-    "1214817156420862012": 50,
+	"1214817156420862012": 50,
 };
 
-async function setMemberCount() {
-    console.log("Fetching member count");
-    
-    const promises = [
-        fetch("https://discord.com/api/v9/invites/suyu?with_counts=true&with_expiration=true"),
-        DISCORD_USER_TOKEN
-            ? fetch("https://discord.com/api/v9/guilds/1214371687114477618/roles/member-counts", {
-                headers: {
-                    Authorization: DISCORD_USER_TOKEN,
-                },
-            })
-            : Promise.resolve({ json: () => roleMembers }),
-        GITLAB_API_TOKEN
-            ? fetch("https://gitlab.com/api/v4/projects/suyu-emu%2Fsuyu/", {
-                headers: {
-                    Authorization: `Bearer ${GITLAB_API_TOKEN}`,
-                },
-            })
-            : Promise.resolve({ json: () => ({ star_count: 0 }) }), // Default to 0 if no token is provided
-    ];
+async function fetchServerSideData() {
+	console.log("Fetching member count");
 
-    const [res, roles, gitlabRes] = await Promise.all(promises);
-    const jsonPromises = [res.json(), roles.json(), gitlabRes.json()];
-    const [resJson, rolesJson, gitlabResJson] = await Promise.all(jsonPromises);
+	const promises = [
+		fetch("https://discord.com/api/v9/invites/suyu?with_counts=true&with_expiration=true"),
+		DISCORD_USER_TOKEN
+			? fetch("https://discord.com/api/v9/guilds/1214371687114477618/roles/member-counts", {
+					headers: {
+						Authorization: DISCORD_USER_TOKEN,
+					},
+				})
+			: Promise.resolve({ json: () => roleMembers }),
+		GITLAB_API_TOKEN
+			? fetch("https://gitlab.com/api/v4/projects/suyu-emu%2Fsuyu/", {
+					headers: {
+						Authorization: `Bearer ${GITLAB_API_TOKEN}`,
+					},
+				})
+			: Promise.resolve({ json: () => ({ star_count: 0 }) }), // Default to 0 if no token is provided
+	];
 
-    memberCount = resJson.approximate_member_count;
-    starCount = gitlabResJson.star_count;
-    if (DISCORD_USER_TOKEN) roleMembers = rolesJson;
+	const [res, roles, gitlabRes] = await Promise.all(promises);
+	const jsonPromises = [res.json(), roles.json(), gitlabRes.json()];
+	const [resJson, rolesJson, gitlabResJson] = await Promise.all(jsonPromises);
 
-    console.log("Member count:", memberCount);
-    console.log("Stars count:", starCount);
+	memberCount = resJson.approximate_member_count;
+	starCount = gitlabResJson.star_count;
+	if (DISCORD_USER_TOKEN) roleMembers = rolesJson;
+
+	console.log("Member count:", memberCount);
+	console.log("Stars count:", starCount);
 }
 
 if (!building) {
-    await setMemberCount();
-    setInterval(setMemberCount, 1000 * 60 * 10);
+	await fetchServerSideData();
+	setInterval(fetchServerSideData, 1000 * 60 * 10);
 }
 
 export async function load(opts) {
-    return {
-        memberCount,
-        starCount,
-        roleMembers,
-    };
+	return {
+		memberCount,
+		starCount,
+		roleMembers,
+	};
 }
